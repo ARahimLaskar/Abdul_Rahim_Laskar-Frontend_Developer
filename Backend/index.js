be9d614eec7e749871a8efd20c230eee49da7b9e
ARahimLaskar <arlaskar1002@gmail.com>
2023-09-06 00:50:15 +0530

    updated

---
+++ Backend/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const cors = require("cors");
+require("dotenv").config();
+
+const { connection } = require("./ConfigDataBase/db.js");
+const userRoutes = require("./Routes/userRoutes.js");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.use("/user", userRoutes);
+
+const PORT = 8000;
+
+app.listen(PORT, async () => {
+  try {
+    await connection;
+    console.log("db connected");
+  } catch (error) {
+    console.log("error connecting db " + error);
+  }
+  console.log("server running at port " + PORT);
+});
